5ce7050906f0f3da5289dac1e944d29c593f9fb7
Melih Serin <melihsrnn@gmail.com>
2025-04-04 19:11:29 +0200

    Creating Score Entity with its corresponding Repository, DTO object, Mapper files: Closing Issue #122

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Score.java
@@ -0,0 +1,78 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "score")
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id", nullable = false)
+    @JsonIgnore
+    private Quiz quiz; 
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user; 
+
+    @Column(nullable = true)
+    private int correctQuestions;
+
+    @Column(nullable = true)
+    private int totalQuestions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getCorrectQuestions() {
+        return correctQuestions;
+    }
+
+    public void setCorrectQuestions(int correctQuestions) {
+        this.correctQuestions = correctQuestions;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+}
